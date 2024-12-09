aab66dfa5d7fdd2018e0d1e7940cac7eb9d2a2ff
Zoobia Syyed <syyedzoobia@gmail.com>
2024-12-08 16:18:58 -0800

    db prob 3

--- Sliding-Window.ts
+++ Sliding-Window.ts
@@ -25,3 +25,38 @@ var findLength = function(nums, k) {
 
     return ans;
 }
+
+//Example 2: You are given a binary string s (a string containing
+//only "0" and "1"). You may choose up to one "0" and flip it to a
+//"1". What is the length of the longest substring achievable that
+//contains only "1"?
+
+//For example, given s = "1101100111", the answer is 5.
+//If you perform the flip at index 2, the string becomes 1111100111.
+
+//our constaint is how many 0s are in the window, can have at most
+//one zero in the window at any given time
+
+var findLengthBinary = function (s : string) {
+  let left = 0;
+  let curr = 0;
+  let ans = 0;
+
+  for (let right = 0; right < s.length; right++){
+    if (s[right] === "0"){
+      curr += 1;
+    }
+
+    while (curr > 1){
+      if (s[left] === "0"){
+        curr -= 1;
+
+      }
+      left ++;
+    }
+
+     ans = Math.max(ans, right - left + 1);
+  }
+
+  return ans; 
+};
