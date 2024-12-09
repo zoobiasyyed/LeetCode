05710f5a7f731f3b649eeb1c78f8dbea5bed3318
Zoobia Syyed <syyedzoobia@gmail.com>
2024-12-09 14:20:43 -0800

    db prob 8

---
+++ Min-Value-For-Positive.ts
@@ -0,0 +1,11 @@
+function minStartValue(nums: number[]): number {
+  let totalSum = 0;
+  let startValue = 1;
+  let minSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    totalSum += nums[i];
+    minSum = Math.min(minSum, totalSum);
+  }
+  startValue = 1 - minSum;
+  return startValue;
+}
